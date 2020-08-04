d9c659a7f26502ec4e351f71095bd3c7a2a5d357
chalikm <chalikfroza@gmail.com>
2020-08-04 21:57:51 +0900

    day 1 part 2

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,68 @@
   <v-app>
     <v-main>
       <v-container>
-        <v-row text-center>
-          <v-col cols="5">
-            <v-select
-              v-model="Countrie"
-              id="Countrie"
-              :items="Countries"
-              :rules="[v => !!v || 'Item is required']"
-              label="Countries"
-              required 
-              @change="getOneCovidCountry(Countrie)"
-            ></v-select>
+        <v-row>
+          <v-col>
+            <v-row>
+              <v-col>
+                <div class="text-h4">COVID 19 TRACKER</div>
+              </v-col>
+              <v-col cols="2">
+                <v-select
+                  v-model="Countrie"
+                  id="Countrie"
+                  :items="Countries"
+                  :rules="[v => !!v || 'Item is required']"
+                  label="Countries"
+                  required
+                  @change="getOneCovidCountry(Countrie)"
+                  class="ma-0 pa-0"
+                ></v-select>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-card class="mx-auto pa-5" max-width="250" raised>
+                  <v-list-item three-line>
+                    <v-list-item-content>
+                      <div class="overline mb-4 text-center">CORONA VIRUS CASES</div>
+                      <v-list-item-title class="headline text-center">{{OneCountrieData.TotalConfirmed}}</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-card>
+              </v-col>
+
+              <v-col>
+                <v-card class="mx-auto pa-5" max-width="250" raised>
+                  <v-list-item three-line>
+                    <v-list-item-content>
+                      <div class="overline mb-4 text-center">RECOVERED</div>
+                      <v-list-item-title class="headline text-center">{{OneCountrieData.TotalRecovered}}</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-card>
+              </v-col>
+
+              <v-col>
+                <v-card class="mx-auto pa-5" max-width="250" raised>
+                  <v-list-item three-line>
+                    <v-list-item-content>
+                      <div class="overline mb-4 text-center">DEATHS</div>
+                      <v-list-item-title class="headline text-center">{{OneCountrieData.TotalDeaths}}</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col cols="3">
+            <v-row>
+              <v-col>tes</v-col>
+            </v-row>
           </v-col>
         </v-row>
       </v-container>
-      {{OneCountrieData}}
+      <!-- {{OneCountrieData}} -->
       <HelloWorld />
     </v-main>
   </v-app>
@@ -33,29 +80,30 @@ export default {
   },
 
   data: () => ({
-    Countrie:"",
+    Countrie: "",
     OneCountrieData: null,
-    Countries: [],
+    Countries: ["worldwide"],
     CountriesData: null,
+    worlwideData: null,
   }),
   mounted() {
     this.getCountry();
     this.getDataCovidCountry();
-    
   },
   methods: {
-    getOneCovidCountry(Country){
-      // this.OneCountrieData = Country
-      for (let i = 0; i < this.CountriesData.length; i++) {
-        if(this.CountriesData[i].Country == Country){
-          this.OneCountrieData = this.CountriesData[i]
+    getOneCovidCountry(c) {
+      for (let i = 0; i < this.CountriesData.Countries.length; i++) {
+        if (c == "worldwide") {
+          this.OneCountrieData = this.CountriesData.Global;
         }
-        
+        if (this.CountriesData.Countries[i].Country == c) {
+          this.OneCountrieData = this.CountriesData.Countries[i];
+        }
       }
     },
     getDataCovidCountry() {
       axios.get("https://api.covid19api.com/summary").then((response) => {
-        this.CountriesData = response.data.Countries;
+        this.CountriesData = response.data;
       });
     },
     getCountry: function () {
