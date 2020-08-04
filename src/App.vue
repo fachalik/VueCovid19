f9fd5b1604db96ffa7c0ec683417fff2fb1a8160
chalikm <chalikfroza@gmail.com>
2020-08-04 14:03:52 +0900

    day one part one

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,70 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <v-row text-center>
+          <v-col cols="5">
+            <v-select
+              v-model="Countrie"
+              id="Countrie"
+              :items="Countries"
+              :rules="[v => !!v || 'Item is required']"
+              label="Countries"
+              required 
+              @change="getOneCovidCountry(Countrie)"
+            ></v-select>
+          </v-col>
+        </v-row>
+      </v-container>
+      {{OneCountrieData}}
+      <HelloWorld />
     </v-main>
-    {{info}}
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 import axios from "axios";
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     HelloWorld,
   },
 
   data: () => ({
-    info: null
+    Countrie:"",
+    OneCountrieData: null,
+    Countries: [],
+    CountriesData: null,
   }),
-  mounted(){
-    axios
-    .get('https://api.covid19api.com/summary')
-      .then(response => (this.info = response.data))
+  mounted() {
+    this.getCountry();
+    this.getDataCovidCountry();
+    
+  },
+  methods: {
+    getOneCovidCountry(Country){
+      // this.OneCountrieData = Country
+      for (let i = 0; i < this.CountriesData.length; i++) {
+        if(this.CountriesData[i].Country == Country){
+          this.OneCountrieData = this.CountriesData[i]
+        }
+        
+      }
+    },
+    getDataCovidCountry() {
+      axios.get("https://api.covid19api.com/summary").then((response) => {
+        this.CountriesData = response.data.Countries;
+      });
+    },
+    getCountry: function () {
+      axios.get("https://api.covid19api.com/summary").then((r) => {
+        for (let i = 0; i < r.data.Countries.length; i++) {
+          this.Countries.push(r.data.Countries[i].Country);
+        }
+      });
+    },
   },
 };
 </script>
